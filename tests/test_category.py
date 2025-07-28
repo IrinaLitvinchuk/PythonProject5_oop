ec3d4d98310828646c32e2aca9f02da0fc096263
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-07-28 20:00:33 +0300

    added tests to Category and Product

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,7 @@
 from src.category import Category
 
-def test_init(category1, products):
+def test_init_Category(category1, products):
+    """Проверяет корректность инициализации объектов класса Category"""
     assert category1.name == "Смартфоны"
     assert category1.description == (
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
@@ -9,6 +10,7 @@ def test_init(category1, products):
 
 
 def test_category_and_product_count_reset(products, product4):
+    """"""
     # Сбросим счётчики перед тестом
     Category.category_count = 0
     Category.product_count = 0
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,5 @@
 def test_init(product1):
+    """Проверяет корректность инициализации объектов класса Product"""
     assert product1.name == "Samsung Galaxy S23 Ultra"
     assert product1.description == "256GB, Серый цвет, 200MP камера"
     assert product1.price == 180000.0
