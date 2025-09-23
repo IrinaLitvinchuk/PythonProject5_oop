13d487215f6ce63754f9919ad7f67ffe1f087e0b
Irina Litvinchuk <litvinchuk.irina2909@gmail.com>
2025-09-23 20:52:29 +0300

    added test to product.py

--- src/product.py
+++ src/product.py
@@ -11,12 +11,12 @@ class Product(BaseProduct, PrintMixin):
     def __init__(
         self, name: str, description: str, price: float, quantity: int
     ) -> None:
+        if quantity == 0:
+            raise ValueError('Товар с нулевым количеством не может быть добавлен')
         self.name = name
         self.description = description
         self.__price = price  # приватный атрибут
         self.quantity = quantity
-        if self.quantity == 0:
-            raise ValueError('Товар с нулевым количеством не может быть добавлен')
         super().__init__()
 
 
